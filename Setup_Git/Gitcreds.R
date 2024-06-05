db28b13fcb000c8abd3870e86be8a856195d52f8
Selina <162927999+Refelicus@users.noreply.github.com>
2024-06-05 23:19:52 +0200

    Update Gitcreds.R

--- Setup_Git/Gitcreds.R
+++ Setup_Git/Gitcreds.R
@@ -1,8 +1,11 @@
+# Installation of gitcreds for linking personal information for the use of a GIT repository
+# Run the following commands 
+
 install.packages("gitcreds")
 library(gitcreds)
 gitcreds_set()
 
-# add this in terminal
+# add this in terminal for configuration of personal email and github name
 # git config --global user.email "selina.dreesman@outlook.com"
 # git config --global user.name "Refelicus"
 
